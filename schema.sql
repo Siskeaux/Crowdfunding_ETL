f366192e0fbf08964b55dc45aa6c91ba3dc39332
levimcleod <113212383+levimcleod@users.noreply.github.com>
2023-01-26 14:44:04 -0600

    add schema file

---
+++ schema.sql
@@ -0,0 +1,42 @@
+﻿DROP TABLE IF EXISTS category, subcategory, campaign, contacts;
+
+CREATE TABLE category (
+    category_id VARCHAR NOT NULL,
+    category VARCHAR NOT NULL,
+    PRIMARY KEY (category_id)
+);
+
+CREATE TABLE subcategory (
+    subcategory_id VARCHAR NOT NULL,
+    subcategory VARCHAR NOT NULL,
+    PRIMARY KEY (subcategory_id)
+);
+
+CREATE TABLE campaign (
+    cf_id INT NOT NULL,
+    contact_id INT NOT NULL,
+    company_name VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    goal INT NOT NULL,
+    pledged INT NOT NULL,
+    outcome VARCHAR NOT NULL,
+    backers_count INT NOT NULL,
+    country VARCHAR NOT NULL,
+    currency VARCHAR NOT NULL,
+    launched_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    category_id VARCHAR NOT NULL,
+    subcategory_id VARCHAR NOT NULL,
+    PRIMARY KEY (cf_id),
+    FOREIGN KEY (category_id) REFERENCES category (category_id),
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory (subcategory_id)
+);
+
+CREATE TABLE contacts (
+    contact_id INT   NOT NULL,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    email VARCHAR   NOT NULL,
+    PRIMARY KEY (contact_id),
+    FOREIGN KEY (contact_id) REFERENCES campaign (contact_id)
+);
